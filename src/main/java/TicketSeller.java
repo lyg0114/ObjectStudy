e6b432a321e0e6f5e614f83bdf8c3b1acdb9e40a
lyg0114 <whdnseowkd@naver.com>
2020-10-03 23:14:22 +0900

    modified:   src/main/java/TicketOffice.java 	modified:   src/main/java/TicketSeller.java
    p31~32
    TicketOffice의 자율성을 선택할 것인가, 전체설계의 결합도를 선택할 것인가.!

--- src/main/java/TicketSeller.java
+++ src/main/java/TicketSeller.java
@@ -3,15 +3,18 @@ public class TicketSeller {
     private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
+
         this.ticketOffice = ticketOffice;
     }
 
     public TicketOffice getTicketOffice(){
         return ticketOffice;
+
     }
 
     public void sellTo(Audience audience){
+       //ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+        ticketOffice.sellTicketTo(audience);
 
-       ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
